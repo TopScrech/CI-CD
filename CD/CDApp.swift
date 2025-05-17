50ebb7cb1b9567bf4379371b92ccc22dce06a0af
TopScrech <sergei_saliukov@icloud.com>
2025-05-17 23:41:23 +0200

    Initial Commit

---
+++ CD/CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDApp.swift
+//  CD
+//
+//  Created by Sergei Saliukov on 17.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
