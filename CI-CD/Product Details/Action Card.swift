f16b89ec81914cf2a2c783fb576e6eb971ee08b6
TopScrech <sergei_saliukov@icloud.com>
2025-05-20 22:44:10 +0200

    -

--- CI-CD/Product Details/Action Card.swift
+++ CI-CD/Product Details/Action Card.swift
@@ -80,54 +80,6 @@ struct ActionCard: View {
     }
 }
 
-@Observable
-final class ActionVM {
-    var issues: [CiIssue] = []
-    
-    var warningCount: Int? {
-        issues.filter {
-            $0.attributes?.issueType == .warning
-        }.count
-    }
-    
-    var errorCount: Int? {
-        issues.filter {
-            $0.attributes?.issueType == .error
-        }.count
-    }
-    
-    var analyzerWarningCount: Int? {
-        issues.filter {
-            $0.attributes?.issueType == .analyzerWarning
-        }.count
-    }
-    
-    var testFailureCount: Int? {
-        issues.filter {
-            $0.attributes?.issueType == .testFailure
-        }.count
-    }
-    
-    func buildIssues(_ actionId: String) async throws {
-        guard let provider = try await provider() else {
-            return
-        }
-        
-        let request = APIEndpoint
-            .v1
-            .ciBuildActions
-            .id(actionId)
-            .issues
-            .get()
-        
-        do {
-            issues = try await provider.request(request).data
-        } catch {
-            print(error)
-        }
-    }
-}
-
 //#Preview {
 //    ActionCard()
 //}
