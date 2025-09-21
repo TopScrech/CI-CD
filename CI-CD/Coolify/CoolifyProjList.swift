d5e762cc49f3ecb77fbdc338113b3f01ef47e608
TopScrech <sergei_saliukov@icloud.com>
2025-09-22 00:05:11 +0200

    Created CoolifyProjListVM

--- CI-CD/Coolify/CoolifyProjList.swift
+++ CI-CD/Coolify/CoolifyProjList.swift
@@ -1,9 +1,14 @@
 import SwiftUI
 
 struct CoolifyProjList: View {
+    @State private var vm = CoolifyProjListVM()
+    
     var body: some View {
         List {
             
+        }
+        .refreshableTask {
+            await vm.fetchProjects()
         }
     }
 }
---
+++ CI-CD/Coolify/CoolifyProjListVM.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+@Observable
+final class CoolifyProjListVM {
+    func fetchProjects() async {
+        
+    }
+}
