1a20209d3f2a8031cca40adc4b32eb216c2b895f
TopScrech <sergei_saliukov@icloud.com>
2026-05-26 11:10:16 +0200

    Added a link to the GitHub repo

--- CI-CD/Settings/AppSettings.swift
+++ CI-CD/Settings/AppSettings.swift
@@ -5,6 +5,23 @@ struct AppSettings: View {
         List {
             AppearanceSettingsSection()
             FeedbackSettingsSection()
+            
+            Section {
+                Link(destination: URL(string: "https://github.com/TopScrech/CI-CD")!) {
+                    HStack(spacing: 12) {
+                        Image(.gitHub)
+                            .resizable()
+                            .frame(24)
+                            .clipShape(.circle)
+                        
+                        Text("GitHub")
+                    }
+                }
+                .tint(.primary)
+            } footer: {
+                Text("Bug reports, feature requests & contributions are always welcome!")
+                    .secondary()
+            }
         }
         .scrollIndicators(.hidden)
         .navigationTitle("Settings")
